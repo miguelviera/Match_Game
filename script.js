a91bffe608d956c61414d5193bdc7ba0eae51d40
Miguel <43702390+miguelviera@users.noreply.github.com>
2021-03-01 08:50:43 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 
-
+const imgBack = document.querySelector('.imgBack');
+const match1 = document.querySelectorAll('button.match1');
+const start = document.querySelector('#start');
+let colors = ["blue", "red", "green", "yellow", "brown","blue", "red", "green", "yellow", "brown"];
 start.addEventListener("click", function(){
     colors = shuffle(colors);
     for(let i = 0 ;i < colors.length; i++){
